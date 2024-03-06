6205271c197003820f0967cdee22de86ab5b5fb7
SeanGam13 <seangambanou9@gmail.com>
2024-03-06 11:31:17 +0000

    fixing mistakes

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    if (responseFromAPI) {
-      resolve(responseFromAPI); // resolve with API response
-    } else {
-      reject("Error: API call failed"); // reject with error message
-    }
-  });
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => [resolve, reject]);
 }
-
-export default getResponseFromAPI;
